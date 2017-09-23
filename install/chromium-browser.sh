b366f4be2f6c66999bd0d6d9945d9e644b376e7b
Аббас Гусенов <gusenov@live.ru>
2017-09-23 15:08:50 +0600

    Установка браузера Chromium на Ubuntu.

---
+++ install/chromium-browser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# Chromium is an open-source web browser project started by Google, to provide the source code for the proprietary Google Chrome browser.
+
+sudo apt-get -qq install chromium-browser
