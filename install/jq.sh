971c6852bb793214460db55989a457ae9838c152
Аббас Гусенов <gusenov@live.ru>
2017-09-23 15:07:24 +0600

    Установка jq на Ubuntu.

---
+++ install/jq.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x # echo on
+
+# jq is a lightweight and flexible command-line JSON processor.
+# jq is a very high-level functional programming language with support for backtracking and managing streams of JSON data.
+# It is related to the Icon and Haskell programming languages
+
+sudo apt-get -qq install jq
