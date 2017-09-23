598b84056cacc5159b31e17cfd758e7bb563d80f
Аббас Гусенов <gusenov@live.ru>
2017-09-23 15:07:52 +0600

    Установка curl на Ubuntu.

---
+++ install/curl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# cURL is a computer software project providing a library and command-line tool for transferring data using various protocols.
+
+sudo apt-get -qq install curl
