efd55e6497b40595a3762be45c133cf75774fb86
Аббас Гусенов <gusenov@live.ru>
2017-10-07 22:28:39 +0600

    Установка DB Browser for SQLite на Ubuntu.

---
+++ install/sqlitebrowser.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# DB Browser for SQLite is a high quality, visual, open source tool to create, design, and edit database files compatible with SQLite.
+
+sudo apt-get -qq install sqlitebrowser
