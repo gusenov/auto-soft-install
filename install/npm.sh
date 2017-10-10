e6ecd787f0b7740f98cff89cacf73e536598b1cd
Аббас Гусенов <gusenov@live.ru>
2017-10-10 12:46:46 +0600

    Установка npm на Ubuntu.

---
+++ install/npm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x # echo on
+
+# npm is a package manager for the JavaScript programming language. 
+# It is the default package manager for the JavaScript runtime environment Node.js. 
+# It consists of a command line client, also called npm, and an online database of public and paid-for private packages, called the npm registry.
+
+sudo apt-get -qq install npm
