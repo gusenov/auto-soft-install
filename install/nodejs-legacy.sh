3686227751395e2be01862eeb0401f146263a3e1
Аббас Гусенов <gusenov@live.ru>
2017-10-10 12:47:14 +0600

    Установка Node.js на Ubuntu.

---
+++ install/nodejs-legacy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# Node.js is an open-source, cross-platform JavaScript run-time environment for executing JavaScript code server-side.
+
+sudo apt-get -qq install nodejs-legacy
