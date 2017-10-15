efaffb4b9f649787c5c11deaf1f32a6eb1573531
Аббас Гусенов <gusenov@live.ru>
2017-10-16 00:52:57 +0600

    Установка Kazam Screencaster на Ubuntu.

---
+++ install/kazam.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# Kazam is a simple screen recording program that will capture the content of your screen and record a video file that can be played by any video player that supports VP8/WebM video format.
+
+sudo apt-get -qq install kazam
