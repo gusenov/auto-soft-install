4dcc80bc09e1efdac1859bda987b89cfc169e200
Аббас Гусенов <gusenov@live.ru>
2017-10-17 17:31:28 +0600

    Установка браузера GNOME Web на Ubuntu.

---
+++ install/epiphany-browser.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x # echo on
+
+# GNOME Web (originally called Epiphany from 2003-2012) is a free and open-source web browser for the GNOME desktop environment.
+# The browser was forked from Galeon, after developers' disagreements about Galeon's growing complexity.
+
+sudo apt-get -qq install epiphany-browser
