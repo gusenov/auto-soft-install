3260aadec1553d7ff0e393bd823387312acabf3f
Аббас Гусенов <gusenov@live.ru>
2017-12-01 01:53:36 +0600

    Установка YUI Compressor на Ubuntu.

---
+++ install/yui-compressor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# The YUI Compressor is JavaScript minifier designed to be 100% safe and yield a higher compression ratio than most other tools.
+
+sudo apt-get install yui-compressor
