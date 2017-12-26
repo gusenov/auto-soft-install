cc8c6f7253eb923723bc752cd00a6e25f70226bb
Аббас Гусенов <gusenov@live.ru>
2017-12-26 21:16:48 +0600

    Установка простого графического редактора KolourPaint на Ubuntu (нужен KDE).

---
+++ install/kolourpaint4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -x # echo on
+
+# KolourPaint is a simple drawing and image editing application for KDE. 
+# It aims to be easy to use, providing a level of functionality targeted towards the average user. 
+# It is ideal for common tasks such as drawing simple graphics and touching-up photos.
+# This package is part of the KDE graphics module.
+
+sudo apt-get -qq install kolourpaint4
