11c82213be0c74d9c98a9c47c074a6b7ae648070
Аббас Гусенов <gusenov@live.ru>
2018-01-13 22:25:13 +0600

    Установка Byzanz на Ubuntu.

---
+++ install/byzanz.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# Byzanz is a simple tool to record a running X desktop to an animation suitable for presentation in a web browser.
+
+sudo apt-get -qq install byzanz
