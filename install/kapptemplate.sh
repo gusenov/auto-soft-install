dd47712fd0ce2fef7ecc64a6d2f0abe708cc736c
Аббас Гусенов <gusenov@live.ru>
2018-01-22 19:59:11 +0600

    Установка KAppTemplate на Kubuntu.

---
+++ install/kapptemplate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x # echo on
+
+# KAppTemplate is an application to start development quickly using existing templates providing basic repeatedly written code and a proper structure.
+
+sudo apt-get -qq install kapptemplate
