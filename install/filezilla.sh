a7f96f2272927399fbddb9c34d9bf818458734b2
Аббас Гусенов <gusenov@live.ru>
2018-01-31 01:31:49 +0600

    Установка FileZilla Client на Ubuntu.

---
+++ install/filezilla.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -x  # echo on
+
+# FileZilla Client is a fast and reliable cross-platform FTP, FTPS and SFTP client with lots of useful features and an intuitive graphical user interface.
+
+sudo apt-get -qq install filezilla
