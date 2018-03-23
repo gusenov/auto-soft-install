d2c6e4b4c74f0c6ae4e82c4dd0e1844d3798521a
Аббас Гусенов <gusenov@live.ru>
2018-03-23 17:22:05 +0600

    Установка Graphviz на Ubuntu.

---
+++ install/graphviz.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x  # echo on
+
+# Graphviz is open source graph visualization software.
+# Graph visualization is a way of representing structural information as diagrams of abstract graphs and networks.
+
+sudo apt-get -qq install graphviz
