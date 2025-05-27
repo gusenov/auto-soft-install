039224889d2231b5ce1c9ddf4bb4cdd7c88f7afc
Аббас Гусенов <gusenov@live.ru>
2025-05-27 16:05:40 +0500

    dnf install на Fedora.

---
+++ os/fedora/sudo-dnf-install.sh
@@ -0,0 +1,32 @@
+PACKAGES=(
+    "gnome-pomodoro"
+    "moreutils"
+    "thunderbird"
+    "git-gui"
+    "snapd"
+    "code"
+
+    "xset"
+    "xfce4-xkb-plugin"
+
+    "chromium"
+    "google-chrome-stable"
+
+    "git-remote-gcrypt"
+    "keepassxc"
+    "openssl"
+)
+
+for PACKAGE in ${PACKAGES[@]}; do
+
+    # &> /dev/null — это способ полностью скрыть вывод команды, чтобы он нигде не отображался.
+    # &> — это оператор оболочки, который говорит: «отправить всё (и stdout, и stderr)».
+
+    if dnf list installed "$PACKAGE" &> /dev/null; then
+        :  # встроенная команда Bash для “ничего не делать”
+        # echo "$PACKAGE installed"
+    else
+        # echo "$PACKAGE is not installed"
+        echo "sudo dnf install $PACKAGE"
+    fi
+done
