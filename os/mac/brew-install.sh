dd7293b03fc7826e744b609c1f1ce165c42965df
Аббас Гусенов <gusenov@live.ru>
2026-05-04 18:15:23 +0500

    brew install --cask emacs

--- os/mac/brew-install.sh
+++ os/mac/brew-install.sh
@@ -18,6 +18,7 @@ brew install --cask zed
 brew install --cask eclipse-cpp
 brew install --cask sublime-text
 brew install --cask sublime-merge
+brew install --cask emacs
 
 brew install --cask keepassxc
 brew install --cask libreoffice
