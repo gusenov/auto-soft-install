2ee913d539bd7c378c762c44fc5c7ed88a137df9
Аббас Гусенов <gusenov@live.ru>
2026-05-06 13:25:38 +0500

    Google.gemini-cli-vscode-ide-companion

--- ide/vscode/code-install-extension.sh
+++ ide/vscode/code-install-extension.sh
@@ -31,6 +31,7 @@ EXTENSIONS=(
 
     "anthropic.claude-code"
     "Google.geminicodeassist"
+    "Google.gemini-cli-vscode-ide-companion"
 )
 
 MAC_PATH_TO_CODE="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
